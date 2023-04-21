bd97fad1ec0e1b0127150dc82814b65bc83251d1
Rafael Veiga <rafael_todescato@hotmail.com>
2023-04-21 12:59:09 -0400

    feat: JWT generated as successful.

---
+++ WheresNannyApi.WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,53 @@
+﻿using WheresNannyApi.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using WheresNannyApi.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace WheresNannyApi.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(User user)
+        {
+            var issuer = _configuration.GetValue<string>("JWt:Issuer");
+            var audience = _configuration.GetValue<string>("JWt:Audience");
+            var key = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("JWt:Key"));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(JwtRegisteredClaimNames.Email, user.Username),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+             }),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials
+            (new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            var stringToken = tokenHandler.WriteToken(token);
+            return Ok(stringToken);
+        }
+    }
+}
